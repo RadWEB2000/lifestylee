855b1609141d32443a9e1753ab46151ffa24b132
Radosław Adamczyk <radoslaw.adamczyk2000@gmail.com>
2025-01-16 11:24:07 +0100

    catch / try / throw

--- tw/src/app/[category]/[subcategory]/[slug]/page.tsx
+++ tw/src/app/[category]/[subcategory]/[slug]/page.tsx
@@ -33,35 +33,39 @@ type tPostPage = {
 export default async function PostPage(props: tPostPage) {
   // const slug: string = await props.params.slug;
   const slug = props.params.slug;
+  try {
+    const page = await (
+      await GET_POST(slug ? slug : "kiedy-wybory-prezydenckie-2025-w-polsce")
+    ).page;
 
-  const page = await (
-    await GET_POST(slug ? slug : "kiedy-wybory-prezydenckie-2025-w-polsce")
-  ).page;
-
-  return (
-    <div>
-      <Link href="/">Start</Link>
-      <h1>{`Post: ${page.title}`}</h1>
-      <p>{page.date}</p>
-      <p dangerouslySetInnerHTML={{ __html: page.excerpt }} />
-      <Image
-        alt={page.featuredImage.altText}
-        height={750}
-        src={page.featuredImage.sourceUrl}
-        sizes={page.featuredImage.srcSet}
-        title={page.featuredImage.title}
-        style={{
-          objectFit: "cover",
-          objectPosition: "center",
-        }}
-        width={1200}
-        quality={95}
-      />
-      <main>
-        <section dangerouslySetInnerHTML={{ __html: page.content }} />
-      </main>
-    </div>
-  );
+    return (
+      <div>
+        <Link href="/">Start</Link>
+        <h1>{`Post: ${page.title}`}</h1>
+        <p>{page.date}</p>
+        <p dangerouslySetInnerHTML={{ __html: page.excerpt }} />
+        <Image
+          alt={page.featuredImage.altText}
+          height={750}
+          src={page.featuredImage.sourceUrl}
+          sizes={page.featuredImage.srcSet}
+          title={page.featuredImage.title}
+          style={{
+            objectFit: "cover",
+            objectPosition: "center",
+          }}
+          width={1200}
+          quality={95}
+        />
+        <main>
+          <section dangerouslySetInnerHTML={{ __html: page.content }} />
+        </main>
+      </div>
+    );
+  } catch (error) {
+    console.log(`❌ Error fetch post: ${error}`);
+    throw error;
+  }
 }
 
 // export const dynamicParams = true;
