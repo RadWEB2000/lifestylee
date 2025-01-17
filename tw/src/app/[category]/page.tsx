c1efe424bbb6fba8fa510acf386c461c3c349533
Radosław Adamczyk <radoslaw.adamczyk2000@gmail.com>
2025-01-17 15:38:13 +0100

    work with render dynamic data on category page

--- tw/src/app/[category]/page.tsx
+++ tw/src/app/[category]/page.tsx
@@ -1,11 +1,30 @@
-export function generateStaticParams() {
-  return [];
-}
+import { GET_CATEGORY_PAGE } from "@/data/graphql";
+// import type { Metadata } from "next";
 
-export default function CategoryPage() {
+// interface tPostPage {
+//   params: Promise<{ slug: string }>;
+// }
+
+// export async function generateMetadata(): Promise<Metadata> {
+//   const slug: string = (await props.params).slug;
+//   const seo = (await GET_CATEGORY_PAGE()).seo;
+//   return {
+//     ...seo,
+//     other: {
+//       jsonLd: "TEST",
+//     },
+//   };
+// }
+
+export default async function CategoryPage() {
+  const { page } = await GET_CATEGORY_PAGE();
+
   return (
     <div>
       <h1>Strona kategorii</h1>
+      <h2>{page.title}</h2>
+      <p dangerouslySetInnerHTML={{ __html: page.content }} />
     </div>
   );
 }
+export const dynamicParams = false;
