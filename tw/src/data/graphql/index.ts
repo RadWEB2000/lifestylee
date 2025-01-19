0882bcab640d8b5d7f7c32661ba2c3a7c8ef67e5
Radosław Adamczyk <radoslaw.adamczyk2000@gmail.com>
2025-01-19 16:53:50 +0100

    remove get category page

--- tw/src/data/graphql/index.ts
+++ tw/src/data/graphql/index.ts
@@ -2,4 +2,4 @@ export { default as GET_POST } from "@/queries/GET_POST";
 export { default as GET_MAIN_CATEGORIES } from "@/queries/GET_MAIN_CATEGORIES";
 export { default as GET_POSTS } from "@/queries/GET_POSTS";
 export { default as GET_MAIN_DATA } from "@/queries/GET_MAIN_DATA";
-export { default as GET_CATEGORY_PAGE } from "@/queries/GET_CATEGORY_PAGE";
+// export { default as GET_CATEGORY_PAGE } from "@/queries/GET_CATEGORY_PAGE";
