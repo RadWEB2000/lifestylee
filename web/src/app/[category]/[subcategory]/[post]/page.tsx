879861a256ad22ab727a84b628288467fb06b26d
Radosław Adamczyk <radoslaw.adamczyk2000@gmail.com>
2025-01-24 20:32:09 +0100

    work above routing

--- web/src/app/[category]/[subcategory]/[post]/page.tsx
+++ web/src/app/[category]/[subcategory]/[post]/page.tsx
@@ -2,6 +2,7 @@ export default function PostPage({ post }: { post?: string }) {
   console.log("wpis blogowy: ", post);
   return (
     <div>
+      <ul>Start ➡️ Kategoria ➡️ Podkategoria ➡️ Wpis blogowy</ul>
       <div>
         <h1>Wpis blogowy: {post}</h1>
       </div>
