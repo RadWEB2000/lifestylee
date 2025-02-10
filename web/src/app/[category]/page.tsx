83efc99ce26fff42c4be7f9fd83608af8ebfb2aa
RadWEB2000 <radoslaw.adamczyk2000@gmail.com>
2025-02-10 23:58:41 +0100

    categories

--- web/src/app/[category]/page.tsx
+++ web/src/app/[category]/page.tsx
@@ -2,6 +2,7 @@ import { generateUri } from "@/func/index";
 import GET_CATEGORY from "@/queries/GET_CATEGORY";
 import Link from "next/link";
 import Image from "next/image";
+import { RegularBlogCard } from "@/components/Utils/BlogCards";
 
 type tCategoryPage = {
   params: Promise<{
@@ -16,36 +17,44 @@ type tCategoryPage = {
 export default async function CategoryPage(props: tCategoryPage) {
   const { category } = await props.params;
   const pathname = generateUri(category);
-  const { content, posts, title } = await GET_CATEGORY(pathname);
+  const { page } = await GET_CATEGORY(pathname);
   return (
     <div>
       <div>
-        <h1>Podkategoria: {title}</h1>
+        <h1>Podkategoria: {page.title}</h1>
         <h2>Pathname : {pathname}</h2>
-        <p dangerouslySetInnerHTML={{ __html: content }} />
+        <p dangerouslySetInnerHTML={{ __html: page.content }} />
       </div>
-      <ul>
-        {posts &&
-          posts.map((item) => {
+     {
+      page.subcategories &&
+      <main>
+        {
+          page.subcategories.map((item) => {
             return (
-              <Link href={item.uri} key={item.title} title={item.title}>
-                <Image
-                  alt={item.image.altText}
-                  height={531.25}
-                  src={item.image.sourceUrl}
-                  width={850}
-                  style={{
-                    objectFit: "cover",
-                  }}
-                  title={item.image.title}
-                  quality={55}
-                />
-                <h3>{item.title}</h3>
-                <p dangerouslySetInnerHTML={{ __html: item.excerpt }} />
-              </Link>
-            );
-          })}
-      </ul>
+              <section>
+                <h2>{item.title}</h2>
+                <p dangerouslySetInnerHTML={{__html:item.excerpt}} />
+                {
+                  item.posts &&
+                  <ul>
+                    {
+                      item.posts.map((item) => {
+                        return (
+                          <RegularBlogCard
+                            {...item}
+                            key={item.title}
+                          />
+                        )
+                      })
+                    }
+                  </ul>
+                }
+              </section>
+            )
+          })
+        }
+      </main>
+     }
     </div>
   );
 }
--- web/src/data/graphql/GET_CATEGORY.ts
+++ web/src/data/graphql/GET_CATEGORY.ts
@@ -1,59 +1,146 @@
 import { QueryClient } from "@/lib/clients";
+import { getReleaseDate } from "@/lib/functions";
 import { gql } from "graphql-request";
 
 const GET_CATEGORY_QUERY = gql`
-  query GET_CATEGORY($id: ID!) {
-    category(id: $id, idType: URI) {
-      name
-      uri
+  query GET_CATEGORY_PAGE {
+  category(id: "biznes", idType: URI) {
+    seo {
+      canonicalUrl
       description
-      posts {
-        nodes {
-          uri
-          title(format: RENDERED)
-          excerpt(format: RENDERED)
-          featuredImage {
-            node {
-              altText
-              sourceUrl(size: MEDIUM_LARGE)
-              srcSet(size: MEDIUM)
-              title
+      title
+      openGraph {
+        description
+        title
+        siteName
+      }
+    }
+    name
+    description
+    contentCategoryFields {
+      entry
+    }
+    children(first: 10) {
+      nodes {
+        name
+        uri
+        contentCategoryFields {
+          excerpt
+        }
+        posts(first: 5) {
+             nodes {
+        uri
+        title(format: RENDERED)
+        excerpt(format: RENDERED)
+        featuredImage {
+          node {
+            sourceUrl(size: MEDIUM_LARGE)
+            srcSet(size: THUMBNAIL)
+            title(format: RENDERED)
+            altText
+          }
+        }
+        date
+        categories(first: 1) {
+          nodes {
+            name
+            uri
+          }
+        }
+        postFields {
+          mainCategory(first: 1) {
+            nodes {
+              name
+              uri
             }
           }
         }
       }
+        }
+      }
     }
   }
+}
 `;
 
 type GET_CATEGORY_REQUEST = {
   category: {
-    name: string;
-    uri: string;
-    description: string;
-    posts: {
-      nodes: Array<{
-        uri: string;
-        title: string;
-        excerpt: string;
-        featuredImage: {
-          node: T_WP_FEATURED_IMAGE;
-        };
-      }>;
+    seo: {
+      canonicalUrl:string;
+      description:string;
+      title:string;
+      openGraph :{
+        description:string;
+        title:string;
+        siteName:string;
+      }
+    }
+    name:string;
+    description:string;
+    contentCategoryFields :{
+      entry:string;
     };
+    children:{
+      nodes:Array<{
+        name:string;
+        uri:string;
+        contentCategoryFields :{
+          excerpt:string;
+        };
+        posts:{
+          nodes:Array<{
+            uri:string;
+            title:string;
+            excerpt:string;
+            featuredImage :{
+              node:T_WP_FEATURED_IMAGE;
+            };
+            date:string;
+            categories:{
+              nodes:Array<T_WP_TAXONOMY>;
+            };
+            postFields : {
+              mainCategory :{
+                nodes:Array<T_WP_TAXONOMY>
+              }
+            }
+          }>
+        }
+      }>
+    }
   };
 };
 
 type GET_CATEGORY_RESPONSE = {
-  title: string;
-  uri: string;
-  content: string;
-  posts: Array<{
-    uri: string;
-    title: string;
-    excerpt: string;
-    image: T_WP_FEATURED_IMAGE;
-  }>;
+ seo : {
+  canonicalUrl:string;
+      description:string;
+      title:string;
+      openGraph :{
+        description:string;
+        title:string;
+        siteName:string;
+      }
+ };
+ page : {
+  title:string;
+  content:string;
+  entry:string;
+  subcategories:Array<{
+    title:string;
+    uri:string;
+    excerpt:string;
+    posts:Array<{
+      uri:string;
+      title:string;
+      excerpt:string;
+      image:T_WP_FEATURED_IMAGE;
+      release:string;
+      category:T_WP_TAXONOMY;
+      subcategory:T_WP_TAXONOMY;
+    }>
+  }>
+ }
 };
 
 export default async function GET_CATEGORY(pathname: string) {
@@ -71,17 +158,35 @@ export default async function GET_CATEGORY(pathname: string) {
     );
 
     const response: GET_CATEGORY_RESPONSE = {
-      content: request.category.description,
-      title: request.category.name,
-      uri: request.category.uri,
-      posts: request.category.posts.nodes.map((item) => {
-        return {
-          uri: item.uri,
-          title: item.title,
-          excerpt: item.excerpt,
-          image: item.featuredImage.node,
-        };
-      }),
+      page: {
+        content:request.category.description,
+        entry:request.category.contentCategoryFields.entry,
+        title:request.category.name,
+        subcategories:request.category.children.nodes.map((item) => {
+          return {
+            excerpt:item.contentCategoryFields.excerpt,
+            title:item.name,
+            uri:item.uri,
+            posts:item.posts.nodes.map((item) => {
+              return {
+                uri:item.uri,
+                title:item.title,
+                excerpt:item.excerpt,
+                image:item.featuredImage.node,
+                release:getReleaseDate({
+                  date:item.date,
+                  format:"short"
+                }),
+                category:item.postFields.mainCategory.nodes[0],
+                subcategory:item.categories.nodes[0]
+              }
+            })
+          }
+        })
+      },
+      seo:{
+        ...request.category.seo
+      }
     };
 
     return response;
