0004dac27ff410422ec04be43f71b9efddd643b5
RadWEB2000 <radoslaw.adamczyk2000@gmail.com>
2025-04-30 15:59:03 +0200

    add gtm

--- finalv3/src/app/layout.tsx
+++ finalv3/src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Navigation } from "@/layout/Navigation";
 import GET_MAIN_NAVIGATION from "@/data/queries/GET_MAIN_NAVIGATION";
 import { MenuProvider } from "@/provider";
 import Script from "next/script";
+import Clarity from '@microsoft/clarity';
 
 
 const inter = Inter({
@@ -28,23 +29,32 @@ export default async function RootLayout({
     <html lang="pl_PL">
       <head>
         <link rel="canonical" href="" />
+        <meta name="google-site-verification" content="Ff8M9SgTYgIi3oyAqDXMx1nZOJsOzMLLQBE3dEB1sPc" />
         <Script
-          id="clarity-script"
+          id="gtm-script"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "rc8xjj2n9v");
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5J59KVQ2');
             `,
           }}
         />
-        <meta name="google-site-verification" content="Ff8M9SgTYgIi3oyAqDXMx1nZOJsOzMLLQBE3dEB1sPc" />
       </head>
       <MenuProvider>
         <body className={`${inter.className} antialiased`}>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5J59KVQ2"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
           <Navigation menu={menu} />
           {children}
         </body>
