0b19717ca3a8a47573953a77f50172d9a3b5e6fb
RadWEB2000 <radoslaw.adamczyk2000@gmail.com>
2025-05-03 12:00:53 +0200

    reject unauthorized

--- finalv3/src/app/robots.txt/route.ts
+++ finalv3/src/app/robots.txt/route.ts
@@ -2,6 +2,7 @@ export const dynamic = 'force-dynamic'
 
 
 export function GET() {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
   const content = `
     User-agent: *
     Allow: /
--- finalv3/src/app/sitemap.xml/route.ts
+++ finalv3/src/app/sitemap.xml/route.ts
@@ -2,6 +2,7 @@ export const dynamic = 'force-dynamic'
 
 
 export async function GET() {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
   const baseUrl = 'https://lifeestylee.pl'
   const apiUrl = 'https://cms.lifeestylee.pl/wp-json/wp/v2'
 
